aaa0b460126ff01fc7b8f727696ee96bc35c84b7
Gregorio Juliana <gregojquiros@gmail.com>
2016-04-15 14:12:49 +0200

    Fully functional

---
+++ server.js
@@ -0,0 +1,89 @@
+﻿var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('./config.cnf', 'utf8').toString());
+exports.config = config;
+
+var logger = require('./utils/logger.js'),
+    math = require('./utils/math.js'),
+    net = require('net'),
+    winston = require('winston'),
+    obj = require('./utils/obj.js'),
+    systemLogger = winston.loggers.get('system');
+
+var clients = {};
+
+var processRequest = function (client, data) {
+    var commands = data.toString().split(" ");
+    switch (commands[0]) {
+        case "SETUP": {
+            var k = JSON.parse(commands[1]).k;
+            var status = math.setupFFS(k);
+            obj.merge(client.status, status);
+            systemLogger.info("Client: " + client.id + " requested identification. Generating p,q,n,s,v.");
+            return { n: status.n, v: status.v, k: status.k }
+        }
+        case "X": {
+            var status = math.generateX(client.status.n);
+            obj.merge(client.status, status);
+            systemLogger.info("Client: " + client.id + " requested x generation.");
+            return { x: status.x };
+        }
+        case "FX": {
+            var status = math.generateFX(client.status.n, client.status.v);
+            obj.merge(client.status, status);
+            systemLogger.info("Generating fake x for client: " + client.id);
+            return { x: status.fx };
+        }
+        case "A": {
+            var a = JSON.parse(commands[1]);
+            obj.merge(client.status, a);
+            systemLogger.info("Client: " + client.id + " sent a.");
+            return a;
+        }
+        case "Y": {
+            var status = math.generateY(client.status.r, client.status.s, client.status.a, client.status.n);
+            obj.merge(client.status, status);
+            systemLogger.info("Client: " + client.id + " requested y generation.");
+            return { y: status.y };
+        }
+        case "FY": {
+            systemLogger.info("Sending fake y for client: " + client.id);
+            return { y: client.status.fy };
+        }
+        case "RESET": {
+            client.status = {};
+            systemLogger.info("Resetting client: " + client.id + " status");
+            return {};
+        }
+    }
+}
+
+net.createServer(function (socket) {
+    
+    systemLogger.info("Client connected");
+    
+    socket.write("Welcome\n");
+    
+    var client = { socket: socket, status: {}, id: obj.generateId() };
+    
+    clients[client.id] = client;
+    
+    socket.on('data', function (data) {
+        try {
+            systemLogger.debug(data.toString());
+        } catch (err) { }
+        var response = processRequest(client, data);
+        socket.write(JSON.stringify(response));
+    });
+    
+    socket.on('end', function () {
+        systemLogger.info("Client: " + client.id + " disconnected");
+        delete clients[client.id];
+    });
+
+    socket.on('error', function () {
+        systemLogger.error("Connection with client: " + client.id + " dropped");
+        delete clients[client.id];
+    });
+
+}).listen(config.port);
+
---
+++ utils/math.js
@@ -0,0 +1,167 @@
+﻿var bigInt = require('big-integer'),
+    server = require('../server.js'),
+    fs = require('fs'),
+    winston = require('winston');
+
+function setCharAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    var result = str.substr(0, index) + chr + str.substr(index + 1);
+    return result;
+}
+
+function byteString(n) {
+    if (n < 0 || n > 255 || n % 1 !== 0) {
+        throw new Error(n + " does not fit in a byte");
+    }
+    return ("000000000" + n.toString(2)).substr(-8)
+}
+
+var bigSqrt = function (n) {
+    n = bigInt(n);
+    if (n.compare(1) == 0) return 1;
+    var x = n.divide(2);
+    while (true) {
+        var y = x.add(n.divide(x)).shiftRight(1);
+        if (y.compare(x) >= 0) {
+            return x;
+        }
+        x = y;
+    }
+}
+
+var randomPrimeSearch = function (max) {
+    var primes = [];
+    for (var i = 0; i < max; i++) {
+        var bigPrime;
+        do {
+            bigPrime = bigInt.randBetween("1e100", "1e200");
+        } while(!bigPrime.isProbablePrime(50))
+        primes.push(bigPrime.toString());
+    }
+    return primes;
+}
+
+var erathostenesSieve = function (max) {
+    var n = bigInt(max), primeIndexes = new Buffer(n.divide(8).add(1).toJSNumber()), upperLimit = bigSqrt(bigInt(max)), primes = [];
+    
+    primeIndexes.fill(85);
+    primeIndexes[0] = parseInt("01110101", 2);
+    
+    for (var i = bigInt(2); i.compare(upperLimit) <= 0; i = i.next()) {
+        var checkbit = i.divmod(8);
+        var byteIndex = checkbit.quotient.toJSNumber();
+        var bitIndex = checkbit.remainder.toJSNumber();
+        if (byteString(primeIndexes[byteIndex]).charAt(bitIndex) == '1') {
+            for (var j = i.multiply(i); j.compare(n) < 0; j = j.add(i)) {
+                var modifybit = j.divmod(8);
+                byteIndex = modifybit.quotient.toJSNumber();
+                bitIndex = modifybit.remainder.toJSNumber();
+                var byte = byteString(primeIndexes[byteIndex]);
+                byte = setCharAt(byte, bitIndex, '0');
+                primeIndexes[byteIndex] = parseInt(byte, 2);
+            }
+        }
+    }
+    
+    for (var i = bigInt(2); i.compare(n) < 0; i = i.next()) {
+        var checkbit = i.divmod(8);
+        var byteIndex = checkbit.quotient.toJSNumber();
+        var bitIndex = checkbit.remainder.toJSNumber();
+        if (byteString(primeIndexes[byteIndex]).charAt(bitIndex) == '1') {
+            primes.push(bigInt(byteIndex).multiply(8).add(bitIndex).toString());
+        }
+    }
+    return primes;
+};
+
+var primeGenerator = function (max, method) {
+    if (method == "sieve")
+        return erathostenesSieve(max);
+    else
+        return randomPrimeSearch(max);
+}
+
+var systemLogger = winston.loggers.get("system");
+
+if (fs.existsSync('./precalc/primes.json'))
+    systemLogger.info("Precalculated primes found");
+else {
+    systemLogger.info("Generating primes...");
+    fs.writeFileSync('./precalc/primes.json', JSON.stringify(primeGenerator(server.config.max, server.config.primeSearch), null, 4));
+}
+
+systemLogger.info("Loading primes");
+var primes = JSON.parse(fs.readFileSync('./precalc/primes.json').toString());
+systemLogger.info("Primes loaded");
+
+var generateCoprime = function (n) {
+    var coprime;
+    
+    do {
+        coprime = bigInt.randBetween(1, n);
+    } 
+	while (bigInt.gcd(n, coprime) != 1);
+    
+    return coprime;
+}
+
+var setupFFS = function (k) {
+    var p = bigInt(primes[bigInt.randBetween(0, primes.length - 1)]);
+    var q = bigInt(primes[bigInt.randBetween(0, primes.length - 1)]);
+    var n = p.multiply(q);
+    var s = [];
+    var v = [];
+    for (var i = 0; i < k; i++) {
+        var si = generateCoprime(n);
+        var vi = si.square().mod(n);
+        v.push(vi.toString());
+        s.push(si.toString())
+    }
+    return { n: n.toString(), s: s, v: v, k: k.toString() };
+}
+
+var generateX = function (n) {
+    n = bigInt(n);
+    var x, r;
+    do {
+        r = bigInt.randBetween(1, n);
+        var s = bigInt.randBetween(-1, 1);
+        x = s.multiply(r.square()).mod(n);
+    } while (x.compare(0) == 0);
+    return { x: x.toString(), r: r.toString() }
+}
+
+var generateFX = function (n, v) {
+    n = bigInt(n);
+    var fx;
+    do {
+        var fy = bigInt.randBetween(1, n);
+        fx = fy.square();
+        for (var i = 0; i < v.length; i++) {
+            var vi = bigInt(v[i]);
+            var ai = bigInt.randBetween(-1, 0);
+            fx = vi.pow(ai).multiply(fx);
+        }
+        fx = fx.mod(n);
+    } while (fx.compare(0) == 0);
+    return { fx: fx.toString(), fy: fy.toString() }
+}
+
+var generateY = function (r, s, a, n) {
+    var y = bigInt(r);
+    n = bigInt(n);
+    for (var i = 0; i < s.length; i++) {
+        var si = bigInt(s[i]);
+        var ai = bigInt(a[i]);
+        y = si.pow(ai).multiply(y);
+    }
+    y = y.mod(n);
+    return { y: y.toString() };
+}
+
+exports.bigSqrt = bigSqrt;
+exports.primeGenerator = primeGenerator;
+exports.setupFFS = setupFFS;
+exports.generateX = generateX;
+exports.generateFX = generateFX;
+exports.generateY = generateY;
